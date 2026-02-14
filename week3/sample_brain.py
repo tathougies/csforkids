cc22e27f480b09c6e7caf34cac18aa71bd00f603
Travis Athougies <travis@athougies.net>
2026-02-14 07:23:35 +0000

    Cleanup

--- week3/sample_brain.py
+++ week3/sample_brain.py
@@ -1,15 +1,15 @@
 # This is a sample duck brain
 
 # You are given:
-#  - north: the obstacle to the north of mother duck
-#  - south: the obstacle to the south of mother duck
-#  - west: the obstacle to the west of mother duck
-#  - east: the obstacle to the east of mother duck
-#  - memory: the number mother duck is remembering
+#  - north: the obstacle to the north of DuckBot
+#  - south: the obstacle to the south of DuckBot
+#  - west: the obstacle to the west of DuckBot
+#  - east: the obstacle to the east of DuckBot
+#  - thought: the number or word DuckBot is remembering
 
 # You must set:
-#  - direction: the direction mother duck should travel
-#  - memory: the number mother duck should remember next
+#  - next_direction: the direction DuckBot should travel
+#  - next_thought: the number DuckBot should remember next
 
 if thought == 0:
     if east == 'x':
